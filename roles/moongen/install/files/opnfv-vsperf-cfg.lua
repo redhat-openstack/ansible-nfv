a5afb83c5128757dcbd65695488078b273e058cf
edannon <edannon@redhat.com>
2016-08-17 09:52:57 +0300

    Adding moongen and testpmd install playbooks
    Change-Id: I33d6b47b61f6944b61caf81d6da050f13d6cfefe

---
+++ roles/moongen/install/files/opnfv-vsperf-cfg.lua
@@ -0,0 +1,14 @@
+VSPERF {
+startRate = 12,
+nrFlows = 256,
+vlanIds = {398},
+testType = "throughput",
+runBidirec = false,
+searchRunTime = 30,
+validationRunTime = 30,
+acceptableLossPct = 0.002,
+frameSize = 64,
+srcMacs = {"a0:36:9f:22:e6:86"},
+dstMacs = {"fa:16:3e:b3:39:c0"},
+ports = {1, 1}
+}
