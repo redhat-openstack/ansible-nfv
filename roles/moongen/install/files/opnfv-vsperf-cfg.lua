709030f14223f342eccc95769e496223e756b3dd
edannon <edannon@redhat.com>
2016-09-04 15:50:07 +0300

    Updating the lua scripts, changing the testpmd flavor to 6 CPUs
    Change-Id: I3da3f4db22db7f2bf513e27acff9b43957c87051

--- roles/moongen/install/files/opnfv-vsperf-cfg.lua
+++ roles/moongen/install/files/opnfv-vsperf-cfg.lua
@@ -1,14 +1,13 @@
 VSPERF {
 startRate = 12,
 nrFlows = 256,
-vlanIds = {398},
+vlanIds = {396,400},
 testType = "throughput",
-runBidirec = false,
+runBidirec = true,
 searchRunTime = 30,
 validationRunTime = 30,
-acceptableLossPct = 0.002,
+acceptableLossPct = 0.0002,
 frameSize = 64,
-srcMacs = {"a0:36:9f:22:e6:86"},
-dstMacs = {"fa:16:3e:b3:39:c0"},
-ports = {1, 1}
+dstMac = "a0:36:9f:22:e6:86",
+ports = {0,1}
 }
