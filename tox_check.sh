e4332e78fedfea3027975d22c7770396833ab3cf
Maxim Babushkin <mbabushk@redhat.com>
2020-03-24 00:37:52 +0200

    Simplify tox_check script (gate script)
    Change-Id: Ic2d7fe08714f19404543e6b8cd9942ee63400b62

--- tox_check.sh
+++ tox_check.sh
@@ -1,38 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
-RUN_ENVS="ansible-lint"
+export TOXENV="ansible-lint"
 
-FAILED=""
-run_tox() {
-    tox -e$1 &> .tox_$1_out &
-}
+python -m tox
 
-verify_tox() {
-    if ! tail -n2 .tox_$1_out | grep -q "$1: commands succeeded"; then
-        FAILED="${FAILED} $1"
-    else
-        echo -e "\033[01;33m_________ output of $1: _________\033[0m"
-        # failed outputs printed at the end
-        cat .tox_$1_out
-        echo -e "\033[01;33m====== ^^ end of $1 ^^ ======\033[0m"
-    fi
-}
-print_failed() {
-    for tEnv in ${FAILED}; do
-        echo -e "\033[01;33m_________ output of ${tEnv}: _________\033[0m"
-        cat .tox_${tEnv}_out;
-        echo -e "\033[01;31m====== ^^ end of failed ${tEnv} ^^ ======\033[0m"
-    done
-}
-
-for tEnv in ${RUN_ENVS}; do run_tox ${tEnv}; done
-wait
 # Delete the .tox/ directory with the venv libs
 rm -rf .tox/
-set +x
-for tEnv in ${RUN_ENVS}; do verify_tox ${tEnv} || exit 1; done
-if [[ ! -z "$FAILED" ]]; then
-    print_failed
-    echo -e "\033[01;31m==== Failed: $FAILED ====\033[0m" >&2
-    exit 1
-fi
