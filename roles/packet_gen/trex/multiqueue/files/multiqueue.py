ba405d13fe41b36a0792dc7982e282d6860b0652
mnietoji <mnietoji@redhat.com>
2021-11-23 00:24:47 +0100

    Multiqueue and autobalance playbook
    The purpose of this playbook is to create the resources that will be
    used by the testcases for testing the feature:
    * trex vm for generating traffic. It will be used multiqueue.py script
    * testpmd vm in which it will be configured several queues
    
    The playbook will generate in trex vm the file /tmp/queues.json which
    contains the following information
    
    [                         --> for each port in trex vm
      {
        "name": "eth1",       --> kernel name for this port
        "rate": 0.8,          --> rate used for testing this port
        "pmd_cpu": 12,        --> pmd cpu usage for the previous rate
        "hypervisor_ports": [ --> hypervisor ports used
          "vhu1532b543-24",   --> virtual port used by testpmd vm
          "dpdk2"             --> physical port used by ovs
        ]
        "queues": {           --> list of queues
          "1": {              --> queue id, as returned by ovs command
            "pps": 0.2,       --> pps (mpps) used to send to this queue
            "ips": [          --> list of ips that will be used
              "48.0.0.1",
              "48.0.0.3",
              "48.0.0.4",
            ],
            "isg": 0          --> delay in us for this stream
          },
          "0": {
            "pps": 0.2,
            "ips": [
              "48.0.0.2",
              "48.0.0.7",
              "48.0.0.8",
            ],
            "isg": 0
          },
          "2": {
            "pps": 0.2,
            "ips": [
              "48.0.0.5",
              "48.0.0.10",
              "48.0.0.15",
            ],
            "isg": 0
          }
        },
      },
      {
        "queues": {
          "1": {
            "pps": 0.2,
            "ips": [
              "48.0.0.3",
              "48.0.0.6",
              "48.0.0.9",
            ],
            "isg": 0
          },
          "0": {
            "pps": 0.2,
            "ips": [
              "48.0.0.1",
              "48.0.0.4",
              "48.0.0.7",
            ],
            "isg": 0
          },
          "2": {
            "pps": 0.2,
            "ips": [
              "48.0.0.2",
              "48.0.0.5",
              "48.0.0.8",
            ],
            "isg": 0
          }
        },
        "pmd_cpu": 6,
        "name": "eth2",
        "rate": 0.4,
        "hypervisor_ports": [
          "vhufbb810f9-40",
          "dpdk3"
        ]
      }
    ]
    
    Change-Id: Ib694e3ab0c13e5ea0d34415a55e179ea25308da7

---
+++ roles/packet_gen/trex/multiqueue/files/multiqueue.py
@@ -0,0 +1,235 @@
+#!/usr/bin/python3
+import sys
+import sys
+import os
+import ipaddress
+import json
+import time
+import argparse
+import os
+import yaml
+import re
+import pprint
+import socket
+import ast
+import signal
+from time import sleep
+sys.path.append('/opt/trex/current/automation/trex_control_plane/server/')
+import outer_packages  # noqa: E402
+from trex.stl.api import *  # noqa: E402
+
+
+def get_nics_from_macs():
+    nic_output = {}
+    nics = socket.if_nameindex()
+    nics = [list(nic)[1] for nic in nics]
+    for nic in nics:
+        with open('/sys/class/net/{}/address'.format(nic), 'r') as f:
+            nic_output[f.read().rstrip('\r\n')] = nic
+    return nic_output
+
+
+def trex_cfg():
+    try:
+        stream = open('/etc/trex_cfg.yaml', 'r')
+        cfg_dict = yaml.safe_load(stream)
+    except Exception as e:
+        print(e)
+        raise e
+
+    nics = get_nics_from_macs()
+
+    devices = []
+    for i in range(len(cfg_dict[0]['port_info'])):
+        device = {}
+        src_mac = cfg_dict[0]['port_info'][i]['src_mac']
+        dst_mac = cfg_dict[0]['port_info'][i]['dest_mac']
+        if src_mac in nics.keys():
+            device['name'] = nics[src_mac]
+        device['dest_mac'] = dst_mac
+        device['src_mac'] = src_mac
+        devices.append(device)
+    return devices
+
+
+def gen_scapy_pkt(devices, maxpps, proto, start_seq=0,
+                  src_port=1025, dst_port=32768, direction=0):
+    ip_start = str(ipaddress.IPv4Address('48.0.0.1')+start_seq)
+    size = 65
+    end_seq = maxpps if maxpps-start_seq <= 1000 else 1000
+    iter = 0
+    for i in range(start_seq, end_seq):
+        print(str(ipaddress.IPv4Address(ip_start)+iter))
+        pkt2 = Ether(src=devices[direction]['src_mac'],
+                     dst=devices[direction]['dest_mac'])
+        pkt1 = pkt2/IP(dst=str(ipaddress.IPv4Address(ip_start)+iter),
+                       src="16.0.0.1")
+        dport = dst_port
+        sport = src_port
+        if direction == 1:
+            pkt1 = pkt2/IP(src=str(ipaddress.IPv4Address(ip_start)+iter),
+                           dst="16.0.0.1")
+            dport = src_port
+            sport = dst_port
+        if proto == 'UDP':
+            pkt = pkt1/UDP(dport=dport, sport=sport)
+        else:
+            pkt = pkt1/TCP(dport=dport, sport=sport)
+        data = (max(0, size - len(pkt))+i) * 'x'
+        sendp(pkt/data, iface=devices[direction]['name'])
+        iter = iter+1
+        sleep(0.05)
+
+
+def parse_testpmd_log(devices, file, start, end):
+    output = []
+    for interface in range(len(devices)):
+        output_item = {'name': devices[interface]['name'], 'queues': {}}
+        output.append(output_item)
+        src_mac = devices[interface]['src_mac']
+        ip_start = str(ipaddress.IPv4Address('48.0.0.1') + start)
+        with open(file, 'r') as tp:
+            for ln in tp:
+                x = re.search('src='+src_mac.upper(), ln)
+                y = re.search('(Receive queue|RSS queue)=0x\d+', ln)
+                z = re.search('sw ptype: L2_ETHER L3_IPV4 L4_UDP ', ln)
+                if x and y and z:
+                    queue = int(y.group().split('=')[1], 16)
+                    len_parsed = int(re.search('length=\d+', ln).
+                                     group().split('=')[1])
+                    value = ipaddress.IPv4Address(ip_start) + (len_parsed - 65)
+                    if queue not in output_item['queues']:
+                        output_item['queues'][queue] = {"pps": 0.2,
+                                                        "isg": 0,
+                                                        "ips": []}
+                    output_item['queues'][queue]["ips"] += [str(value)]
+
+    return(output)
+
+
+class STLImix(object):
+
+    def create_stream(self, pps, vm, src_mac, dst_mac, src_ip, dst_ip,
+                      src_port, dst_port, isg=0, multiplier=1):
+        size = 64
+        base_pkt = Ether(src=src_mac,
+                         dst=dst_mac)/IP(src=src_ip,
+                                         dst=dst_ip)/UDP(dport=dst_port,
+                                                         sport=src_port)
+        pad = max(0, size - len(base_pkt)) * 'x'
+        pkt = STLPktBuilder(pkt=base_pkt/pad,
+                            vm=vm)
+
+        return STLStream(isg=isg,
+                         packet=pkt,
+                         mode=STLTXCont(pps=pps*1000000*float(multiplier)))
+
+    def get_streams(self, src_mac, dst_mac, src_ip, dst_ip, src_port, dst_port,
+                    flowvar, qratio, multiplier, **kwargs):
+        streams = []
+        for queue in qratio.keys():
+            if qratio[queue]["pps"] > 0:
+                vm = STLScVmRaw([
+                    STLVmFlowVar(name=flowvar[0],
+                                 value_list=qratio[queue]["ips"],
+                                 op="inc"),
+                    STLVmWrFlowVar(fv_name=flowvar[0],
+                                   pkt_offset=flowvar[1]),
+                    STLVmFixIpv4(offset='IP'),
+                    STLVmFixChecksumHw(l3_offset="IP",
+                                       l4_offset="UDP",
+                                       l4_type=CTRexVmInsFixHwCs.L4_TYPE_UDP)])
+                streams.append(self.create_stream(float(qratio[queue]["pps"]),
+                                                  vm, src_mac, dst_mac, src_ip,
+                                                  dst_ip, src_port, dst_port,
+                                                  multiplier))
+        return streams
+
+
+def register():
+    return STLImix()
+
+
+def gen_stl(devices, traffic, duration, multiplier):
+    c = STLClient(verbose_level="error")
+    def signal_handler(sig, frame):
+        c.stop(ports=my_ports)
+        c.disconnect()
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
+    passed = True
+    try:
+        c.connect()
+        my_ports = [0, 1]
+        c.reset(ports=my_ports)
+        c.remove_all_streams(my_ports)
+        ports_used = []
+        traffic_ips = ["16.0.0.1", "48.0.0.1"]
+        traffic_ports = [1025, 32768]
+        flow_var = [["dst_ip", 'IP.dst'], ['IP.src', 'IP.src']]
+        for port in my_ports:
+            streams = register().get_streams(devices[port]['src_mac'],
+                                             devices[port]['dest_mac'],
+                                             traffic_ips[(port+2) % 2],
+                                             traffic_ips[(port+1) % 2],
+                                             traffic_ports[(port+2) % 2],
+                                             traffic_ports[(port+1) % 2],
+                                             flow_var[(port+2) % 2],
+                                             traffic[port]['queues'],
+                                             multiplier)
+            if len(streams) > 0:
+                c.add_streams(streams, ports=[port])
+                ports_used.append(port)
+        my_ports = ports_used
+        c.start(ports=my_ports, mult="1", duration=duration)
+        c.wait_on_traffic(ports=my_ports)
+    except STLError as e:
+        passed = False
+        print(e)
+    finally:
+        c.disconnect()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--action", required=True,
+                        choices=['learning', 'parse', 'gen_traffic'],
+                        action='store')
+    parser.add_argument("--packets", nargs='*', type=int, action='store')
+    parser.add_argument('--traffic_json', action='store')
+    parser.add_argument('--log', action='store')
+    parser.add_argument('--duration', type=int, action='store')
+    parser.add_argument('--multiplier', type=int, action='store')
+    parser.add_argument('--pps', action='store')
+    args = parser.parse_args()
+
+    devices = trex_cfg()
+
+    if args.action == "parse":
+        with open(args.traffic_json, 'w') as f:
+            json.dump(parse_testpmd_log(devices,
+                                        args.log,
+                                        args.packets[0],
+                                        args.packets[1]), f, indent=4)
+    elif args.action == "learning":
+        gen_scapy_pkt(devices, args.packets[1],
+                      'UDP', start_seq=args.packets[0])
+        gen_scapy_pkt(devices, args.packets[1],
+                      'UDP', start_seq=args.packets[0], direction=1)
+    elif args.action == "gen_traffic":
+        with open(args.traffic_json, 'r') as f:
+            traffic = json.load(f)
+
+        if args.pps:
+            pps = ast.literal_eval(args.pps)
+            for port in range(len(traffic)):
+                for key, value in traffic[port]['queues'].items():
+                    if key in pps[port].keys():
+                        value['pps'] = pps[port][key]
+                    else:
+                        value['pps'] = 0
+
+
+
+        print(traffic)
+        gen_stl(devices, traffic, args.duration, args.multiplier)
