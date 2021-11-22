4bc62467b21598f71582ea644969cadb5f2a2bec
mnietoji <mnietoji@redhat.com>
2021-11-23 00:24:47 +0100

    Multiqueue and autobalance playbook
    The purpose of this playbook is to create the resources that will be
    used by the testcases for testing the feature:
    * trex vm for generating traffic. It will be used multiqueue.py script
    * testpmd vm in which it will be configured several queues
    
    The playbook will generate in trex vm the file /tmp/queues.json which
    contains the following information
    
    [                         --> for each port in trex vm
      {
        "name": "eth1",       --> kernel name for this port
        "rate": 0.8,          --> rate used for testing this port
        "pmd_cpu": 12,        --> pmd cpu usage for the previous rate
        "hypervisor_ports": [ --> hypervisor ports used
          "vhu1532b543-24",   --> virtual port used by testpmd vm
          "dpdk2"             --> physical port used by ovs
        ]
        "queues": {           --> list of queues
          "1": {              --> queue id, as returned by ovs command
            "pps": 0.2,       --> pps (mpps) used to send to this queue
            "ips": [          --> list of ips that will be used
              "48.0.0.1",
              "48.0.0.3",
              "48.0.0.4",
            ],
            "isg": 0          --> delay in us for this stream
          },
          "0": {
            "pps": 0.2,
            "ips": [
              "48.0.0.2",
              "48.0.0.7",
              "48.0.0.8",
            ],
            "isg": 0
          },
          "2": {
            "pps": 0.2,
            "ips": [
              "48.0.0.5",
              "48.0.0.10",
              "48.0.0.15",
            ],
            "isg": 0
          }
        },
      },
      {
        "queues": {
          "1": {
            "pps": 0.2,
            "ips": [
              "48.0.0.3",
              "48.0.0.6",
              "48.0.0.9",
            ],
            "isg": 0
          },
          "0": {
            "pps": 0.2,
            "ips": [
              "48.0.0.1",
              "48.0.0.4",
              "48.0.0.7",
            ],
            "isg": 0
          },
          "2": {
            "pps": 0.2,
            "ips": [
              "48.0.0.2",
              "48.0.0.5",
              "48.0.0.8",
            ],
            "isg": 0
          }
        },
        "pmd_cpu": 6,
        "name": "eth2",
        "rate": 0.4,
        "hypervisor_ports": [
          "vhufbb810f9-40",
          "dpdk3"
        ]
      }
    ]
    
    Change-Id: Ib694e3ab0c13e5ea0d34415a55e179ea25308da7

---
+++ playbooks/tripleo/multiqueue/README.md
@@ -0,0 +1,105 @@
+# Playbook(s):
+
+* multiqueue_learning: Learn flows received by each queue and pmd cpus for
+  specific rates
+
+The goal of this playbook is to learn about multiqueues:
+* Sending several flows, knowing which flow will arrive to each queue so that
+  we will be able to inject traffic to specific queues
+* Injecting at specific rates, knowing which cpu will the pmd use.
+  Interpolating these values it would be able to estimate cpu usage.
+
+The flow of this playbook is the following one:
+1. pin physical queues to different PMDs. The goal is not to have 2 physical
+   queues of the same port in the same pmd as this will cause that a single
+   virtual queue will be used for the traffic of those physical queues.
+2. Configure dpdk and run testpmd in testpmd vm enabling logs
+3. From trex vm, execute multiqueue.py script to generate several packets.
+   Those packets will arrive to testpmd that will generate testpmd.log with
+   information about the packets received and the queues that received each
+   packet.
+4. Copy testpmd.log to trex vm and execute multiqueue.py to parse it and
+   generates queues.json. This file will have a list of ips for each queue.
+5. Stop testpmd and run again without logs
+6. Configure dpdk, huge pages, trex and run trex in trex vm
+7. Inject from trex in 2 queues placed in different pmd cores at different
+   rates. Get the cpu rate of those pmds.
+8. Add those values to the queues.json file
+9. Remove the cpu pinning done in step 1
+
+testpmd and trex vms together with queues.json file are ready to execute
+multiqueue/autobalance regression from tempest
+
+# Variables:
+
+trex vm cores
+
+```
+trex_lcores: '2-11'
+```
+
+testpmd cpu pinning
+
+```
+testpmd_lcores: '1-7'
+```
+
+testpmd forwarding cores
+
+```
+testpmd_forward_cores: 6
+```
+
+testpmd rx queues
+
+```
+testpmd_rxq: 3
+```
+
+testpmd tx queues
+
+```
+testpmd_txq: 3
+```
+
+testpmd path (for rhel 8.4 vm)
+
+```
+testpmd_bin: "/root/dpdk/build/app/dpdk-testpmd"
+```                                                                                
+
+affinity used for learning mode
+
+```
+pmd_rxq_affinity: [ {'interface': 'dpdk2', 'pmd_rxq_affinity': '0:3,1:5,2:7'},
+                    {'interface': 'dpdk3', 'pmd_rxq_affinity': '0:3,1:5,2:7'} ]
+```                                                                                
+
+multiqueue config
+
+```
+multiqueue_config: True
+```                                                                                
+
+testpmd_log
+
+```
+testpmd_log: /tmp/testpmd.log
+```                                                                                
+
+rates used for learning hypervisor ports (mpps)
+
+```
+port0_rate: 0.8
+port1_rate: 0.4
+```
+
+injection time in seconds
+```
+duration: 40
+```                                                                                
+
+file with the queues learned
+```
+queues_json: "/tmp/queues.json"
+```  
---
+++ roles/packet_gen/trex/multiqueue/files/multiqueue.py
@@ -0,0 +1,353 @@
+#!/usr/bin/python3
+import sys
+import sys
+import os
+import ipaddress
+import json
+import time
+import argparse
+import os
+import yaml
+import re
+import pprint
+import socket
+import ast
+import signal
+from time import sleep
+sys.path.append('/opt/trex/current/automation/trex_control_plane/server/')
+import outer_packages  # noqa: E402
+from trex.stl.api import *  # noqa: E402
+
+
+def get_nics_from_macs():
+    """Get nic names from mac address
+
+    It would look for files /sys/class/net/{nic}/address
+
+    return nics: dict to get nic from mac.
+    """
+    nic_output = {}
+    nics = socket.if_nameindex()
+    nics = [list(nic)[1] for nic in nics]
+    for nic in nics:
+        with open('/sys/class/net/{}/address'.format(nic), 'r') as f:
+            nic_output[f.read().rstrip('\r\n')] = nic
+    return nic_output
+
+
+def trex_cfg():
+    """Get nics from trex cfg file
+
+    Get nics from trex cfg file
+
+    return devices: list with devices including  source and
+                    destination macs
+    """
+    try:
+        stream = open('/etc/trex_cfg.yaml', 'r')
+        cfg_dict = yaml.safe_load(stream)
+    except Exception as e:
+        print(e)
+        raise e
+
+    nics = get_nics_from_macs()
+
+    devices = []
+    for i in range(len(cfg_dict[0]['port_info'])):
+        device = {}
+        src_mac = cfg_dict[0]['port_info'][i]['src_mac']
+        dst_mac = cfg_dict[0]['port_info'][i]['dest_mac']
+        if src_mac in nics.keys():
+            device['name'] = nics[src_mac]
+        device['dest_mac'] = dst_mac
+        device['src_mac'] = src_mac
+        devices.append(device)
+    return devices
+
+
+def gen_scapy_pkt(devices, end_seq, proto, start_seq=0,
+                  src_port=1025, dst_port=32768, direction=0):
+    """Gen scapy packets
+
+    It will generate packets for both interfaces. For one direction
+    it will change source ip while for the other one it will change
+    destination ip. All of the other ips/ports will remain constant.
+    Each packet will have a different packet size so that it will be
+    able to know the ip sent through the packet size. Testpmd is
+    storing in the log packet size and queues information, but not
+    ip address, so ip address is known through the packet size.
+    source or destination ip that is changing will have this range:
+    48.0.0.1 + start_seq until 48.0.0.1 + end_seq
+
+    param devices: devices in which packets will be sent
+    param end_seq: max seq number for packet generation
+    param proto: UDP or TCP
+    param start_seq: start seq number for packet generation
+    param src_port: source port for all packets
+    param dst_port: destination port for all packets
+    param direction: packet direction (0 or 1)
+    """
+    ip_start = str(ipaddress.IPv4Address('48.0.0.1')+start_seq)
+    size = 65
+    iter = 0
+    for i in range(start_seq, end_seq):
+        print(str(ipaddress.IPv4Address(ip_start)+iter))
+        pkt2 = Ether(src=devices[direction]['src_mac'],
+                     dst=devices[direction]['dest_mac'])
+        pkt1 = pkt2/IP(dst=str(ipaddress.IPv4Address(ip_start)+iter),
+                       src="16.0.0.1")
+        dport = dst_port
+        sport = src_port
+        if direction == 1:
+            pkt1 = pkt2/IP(src=str(ipaddress.IPv4Address(ip_start)+iter),
+                           dst="16.0.0.1")
+            dport = src_port
+            sport = dst_port
+        if proto == 'UDP':
+            pkt = pkt1/UDP(dport=dport, sport=sport)
+        else:
+            pkt = pkt1/TCP(dport=dport, sport=sport)
+        data = (max(0, size - len(pkt))+i) * 'x'
+        sendp(pkt/data, iface=devices[direction]['name'])
+        iter = iter+1
+        sleep(0.05)
+
+
+def parse_testpmd_log(devices, file, start_seq):
+    """Parse testpmd log file
+
+    Parse testpmd log file and extract which queue received each
+    ip address
+
+    param devices: devices in which packets were sent
+    param file: testpmd log file
+    param start_seq: start seq number for packet generation
+    return output: for each port and queue, list of ips received
+    """
+    output = []
+    for interface in range(len(devices)):
+        output_item = {'name': devices[interface]['name'], 'queues': {}}
+        output.append(output_item)
+        src_mac = devices[interface]['src_mac']
+        ip_start = str(ipaddress.IPv4Address('48.0.0.1') + start_seq)
+        with open(file, 'r') as tp:
+            for ln in tp:
+                x = re.search('src='+src_mac.upper(), ln)
+                y = re.search('(Receive queue|RSS queue)=0x\\d+', ln)
+                z = re.search('sw ptype: L2_ETHER L3_IPV4 L4_UDP ', ln)
+                if x and y and z:
+                    queue = int(y.group().split('=')[1], 16)
+                    len_parsed = int(re.search('length=\\d+', ln).
+                                     group().split('=')[1])
+                    value = ipaddress.IPv4Address(ip_start) + (len_parsed - 65)
+                    if queue not in output_item['queues']:
+                        output_item['queues'][queue] = {"pps": 0.2,
+                                                        "isg": 0,
+                                                        "ips": []}
+                    output_item['queues'][queue]["ips"] += [str(value)]
+
+    return(output)
+
+
+class STLImix(object):
+
+    def create_stream(self, pps, vm, src_mac, dst_mac, src_ip, dst_ip,
+                      src_port, dst_port, isg=0, multiplier=1):
+        """Create a stream
+
+        Create a trex stream
+
+        param pps: packets per second in mpps
+        param vm: changing parameter in the flow
+        param src_mac: source mac address
+        param dst_mac: destination mac address
+        param src_ip: source ip address
+        param dst_ip: destination ip address
+        param src_port: source port
+        param dst_port: destination port
+        param isg: stream delay
+        param multiplier: multiply to pps
+        return stream: stream generated
+        """
+        size = 64
+        base_pkt = Ether(src=src_mac,
+                         dst=dst_mac)/IP(src=src_ip,
+                                         dst=dst_ip)/UDP(dport=dst_port,
+                                                         sport=src_port)
+        pad = max(0, size - len(base_pkt)) * 'x'
+        pkt = STLPktBuilder(pkt=base_pkt/pad,
+                            vm=vm)
+
+        return STLStream(isg=isg,
+                         packet=pkt,
+                         mode=STLTXCont(pps=pps*1000000*float(multiplier)))
+
+    def get_streams(self, src_mac, dst_mac, src_ip, dst_ip, src_port, dst_port,
+                    flowvar, qratio, multiplier, **kwargs):
+        """Generate a list of streams
+
+        Generate a list of streams
+
+        param src_mac: source mac address
+        param dst_mac: destination mac address
+        param src_ip: source ip address
+        param dst_ip: destination ip address
+        param src_port: source port
+        param dst_port: destination port
+        param flowvar: changing flow parameter
+        param qratio: for each queue, pps and ips are included
+        param multiplier: multiply to pps
+        return streams: List of streams
+        """
+        streams = []
+        for queue in qratio.keys():
+            if qratio[queue]["pps"] > 0:
+                vm = STLScVmRaw([
+                    STLVmFlowVar(name=flowvar[0],
+                                 value_list=qratio[queue]["ips"],
+                                 op="inc"),
+                    STLVmWrFlowVar(fv_name=flowvar[0],
+                                   pkt_offset=flowvar[1]),
+                    STLVmFixIpv4(offset='IP'),
+                    STLVmFixChecksumHw(l3_offset="IP",
+                                       l4_offset="UDP",
+                                       l4_type=CTRexVmInsFixHwCs.L4_TYPE_UDP)])
+                streams.append(self.create_stream(float(qratio[queue]["pps"]),
+                                                  vm, src_mac, dst_mac, src_ip,
+                                                  dst_ip, src_port, dst_port,
+                                                  multiplier))
+        return streams
+
+
+def gen_stl(devices, traffic, duration, multiplier):
+    """Generate traffic with trex
+
+    Connect to trex, generate streams and start injecting traffic
+
+    param devices: nics in which traffic will be generated
+    param traffic: json with information about ports and streams
+    param duration: injection duration in seconds
+    param multiplier: multiply to each pps to increase rate
+    """
+    c = STLClient(verbose_level="error")
+
+    def signal_handler(sig, frame):
+        c.stop(ports=my_ports)
+        c.disconnect()
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
+    passed = True
+    try:
+        c.connect()
+        my_ports = [0, 1]
+        c.reset(ports=my_ports)
+        c.remove_all_streams(my_ports)
+        ports_used = []
+        traffic_ips = ["16.0.0.1", "48.0.0.1"]
+        traffic_ports = [1025, 32768]
+        flow_var = [["dst_ip", 'IP.dst'], ['IP.src', 'IP.src']]
+        for port in my_ports:
+            streams = STLImix().get_streams(devices[port]['src_mac'],
+                                            devices[port]['dest_mac'],
+                                            traffic_ips[(port+2) % 2],
+                                            traffic_ips[(port+1) % 2],
+                                            traffic_ports[(port+2) % 2],
+                                            traffic_ports[(port+1) % 2],
+                                            flow_var[(port+2) % 2],
+                                            traffic[port]['queues'],
+                                            multiplier)
+            if len(streams) > 0:
+                c.add_streams(streams, ports=[port])
+                ports_used.append(port)
+        my_ports = ports_used
+        c.start(ports=my_ports, mult="1", duration=duration)
+        c.wait_on_traffic(ports=my_ports)
+    except STLError as e:
+        passed = False
+        print(e)
+    finally:
+        c.disconnect()
+
+
+if __name__ == '__main__':
+    description = '''
+Multiqueue traffic generator
+Based on this other script:
+https://github.com/yogananth-subramanian/multiqueue_traffic_gen/blob/0bfa30bf4dbe9d10a5539798ae7505168f3adebd/mq.py
+
+It has 3 main functions:
+* learning: Using scapy it generates several packets with different source or
+  destination ip address. Each packet will have different size too so that it
+  can be recognized either by the ip or the packet size. In testpmd it is
+  possible to log each packet received in each queue, but it is not printed
+  out the ip, so using a map between ip and packet size it is possible to know
+  which ip is being received in that queue
+
+* parse: parse testpmd log. It gets which queue received each packet
+
+* gen_traffic: generate traffic using trex, it is possible to send traffic to
+  specific queues
+
+Examples on how to use it:
+* learning:
+  multiqueue.py --packets 0 40
+  It will generate packets from 48.0.0.1 to 48.0.0.40. It will generate traffic
+  in both interfaces, in one interface this will be source ip while in te other
+  it will be destination ip
+
+* parse:
+  multiqueue.py --packets 0 40 --log /tmp/testpmd.log
+                --traffic_json /tmp/queues.json
+  It will parse testpmd log and generate queues.json file with a list of ip
+  address received by each port and queue.
+
+* gen_traffic:
+  multiqueue.py --traffic_json /tmp/queues.json --duration 60 --multiplier 1 \
+                --pps "[{"0": 0.5},{"1": 0.8}]"
+  Using trex it will inject traffic to the specific queues. In this case, it
+  will inject 0.5 mpps to port 0 queue 0 and 0.8 mpps to port 1 queue 1.
+  Ports, queues and flows to used are defined in queues.json file. Injection
+  duration will be 60 seconds. With multiplier parameter it can inject at
+  higher rates, for example, if multiplier would have been 10, injections
+  would be at 5 and 8 mpps.
+'''
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("--action", required=True,
+                        choices=['learning', 'parse', 'gen_traffic'],
+                        action='store')
+    parser.add_argument("--packets", nargs='*', type=int, action='store')
+    parser.add_argument('--traffic_json', action='store')
+    parser.add_argument('--log', action='store')
+    parser.add_argument('--duration', type=int, action='store')
+    parser.add_argument('--multiplier', type=int, action='store')
+    parser.add_argument('--pps', action='store')
+    args = parser.parse_args()
+
+    devices = trex_cfg()
+
+    if args.action == "parse":
+        with open(args.traffic_json, 'w') as f:
+            json.dump(parse_testpmd_log(devices,
+                                        args.log,
+                                        args.packets[0],
+                                        args.packets[1]), f, indent=4)
+    elif args.action == "learning":
+        gen_scapy_pkt(devices, args.packets[1],
+                      'UDP', start_seq=args.packets[0])
+        gen_scapy_pkt(devices, args.packets[1],
+                      'UDP', start_seq=args.packets[0], direction=1)
+    elif args.action == "gen_traffic":
+        with open(args.traffic_json, 'r') as f:
+            traffic = json.load(f)
+
+        if args.pps:
+            pps = ast.literal_eval(args.pps)
+            for port in range(len(traffic)):
+                for key, value in traffic[port]['queues'].items():
+                    if key in pps[port].keys():
+                        value['pps'] = pps[port][key]
+                    else:
+                        value['pps'] = 0
+
+        print(traffic)
+        gen_stl(devices, traffic, args.duration, args.multiplier)
